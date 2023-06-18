0212e0bee7037399b426f51d16e960419562cdc6
Sandeep Urankar <sandeepurankar.98@gmail.com>
2023-06-18 14:16:41 +0530

    Add hashing for password of users.

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const _ = require("lodash");
 const { User, validate } = require("../models/users");
+const bcrypt = require("bcrypt");
 
 router.get("/", async (req, res) => {
   const users = await User.find().sort("name").select("name email");
@@ -15,6 +16,8 @@ router.post("/", async (req, res) => {
   if (user) return res.status(400).send(`User already exist. `);
 
   user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(req.body.password, salt);
   await user.save();
 
   res.status(200).send(_.pick(user, ["_id", "name", "email"]));
